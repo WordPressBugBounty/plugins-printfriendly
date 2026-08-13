ca7f4af8f5dcc9f0d4f9c5d7fd6ddf0a64a6601f
Sean Murphy <sean@iamseanmurphy.com>
2026-08-13 19:17:38 +0000

    5.5.12

--- printfriendly/pf.php
+++ printfriendly/pf.php
@@ -5,7 +5,7 @@
     Plugin URI: https://www.printfriendly.com
     Description: PrintFriendly & PDF button for your website. Optimizes your pages and brand for print, pdf, and email.
     Name and URL are included to ensure repeat visitors and new visitors when printed versions are shared.
-    Version: 5.5.11
+    Version: 5.5.12
     Author: Print, PDF, & Email by PrintFriendly
     Author URI: https://www.printfriendly.com
     License: GPLv2 or later
@@ -46,7 +46,7 @@ if (! class_exists('PrintFriendly_WordPress')) {
          *
          * @var string
          */
-        var $plugin_version = '5.5.11';
+        var $plugin_version = '5.5.12';
         /**
          * The hook, used for text domain as well as hooks on pages and in get requests for admin.
          *
--- printfriendly/readme.txt
+++ printfriendly/readme.txt
@@ -2,8 +2,8 @@
 Contributors: printfriendly, joostdevalk, jrf, rozroz
 Tags: print, pdf, email, print button, pdf button
 Requires at least: 4.9
-Tested up to: 7.0
-Stable tag: 5.5.11
+Tested up to: 7.1
+Stable tag: 5.5.12
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
@@ -144,6 +144,9 @@ Use the option in the Plugin settings. [See how](https://support.printfriendly.c
 
 
 == Changelog ==
+
+= 5.5.12 =
+* Tested against WordPress 7.1
 
 = 5.5.11 =
 * Security fix: prevent stored XSS via the Content Position CSS (button alignment) option (CVE-2026-9738).
